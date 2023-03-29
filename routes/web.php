741acf627fe62c6143d3da3771144a41f785cf91
hiroki1005 <127973982+hiroki1005@users.noreply.github.com>
2023-03-29 22:37:18 +0900

    Revert "初期設定・基礎実装"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +13,6 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
